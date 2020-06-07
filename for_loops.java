1d2356484a28d3df72d12d8ee7dafb13ccdbd030
RealRoRo <rkwarrier1234@gmail.com>
2020-06-07 20:57:34 +0530

    again Done with forloops

--- for_loops.java
+++ for_loops.java
@@ -1,9 +1,7 @@
 class for_loops	{
 	public static void main(String args[]){
 		for(int i = 1;i <= 10;i++)	{
-			System.out.println(i);
-			
-			
+			System.out.println(i);			
 			System.out.println("Done with for loops");
 		}
 	}
