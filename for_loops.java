e7a129ea638692eac256f0f690a4cff5e922292b
RealRoRo <rkwarrier1234@gmail.com>
2020-06-07 20:46:49 +0530

    updated forloops

--- for_loops.java
+++ for_loops.java
@@ -2,6 +2,9 @@ class for_loops	{
 	public static void main(String args[]){
 		for(int i = 1;i <= 10;i++)	{
 			System.out.println(i);
+			
+			
+			System.out.println("Done with for loops")
 		}
 	}
 }
